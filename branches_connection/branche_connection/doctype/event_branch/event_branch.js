fb97dc46636b6ddd53ec178e5abd0aa28d700883
AhmedAbokhatwa <ahmedreda199785@gmail.com>
2024-12-02 16:15:43 +0200

    new_doc: Event Branch

---
+++ branches_connection/branche_connection/doctype/event_branch/event_branch.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Ahmed Reda Abu-khatwa and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Event Branch", {
+// 	refresh(frm) {
+
+// 	},
+// });
