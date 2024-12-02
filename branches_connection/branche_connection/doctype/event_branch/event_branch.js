f44a6227a19a88152199352ad067943af75bf525
AhmedAbokhatwa <ahmedreda199785@gmail.com>
2024-12-02 22:02:51 +0200

    solve save event branch ,as Event producers

--- branches_connection/branche_connection/doctype/event_branch/event_branch.js
+++ branches_connection/branche_connection/doctype/event_branch/event_branch.js
@@ -2,28 +2,16 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Event Branch", {
-  after_save:function (frm){
-    console.log("Start !")
-    frappe.call({
-      method:"event_streaming.event_streaming.doctype.event_consumer.event_consumer.register_consumer",
-      args: {
-        data: frm.doc,
-      },
-      callback: function (r) {
-        // frm.refresh_field("submission_status");
-        console.log("Start Ended !!")
-      },
-    })
-  },
+
 	refresh: function (frm) {
-		frm.set_query("ref_doctype", "producer_doctypes", function () {
-			return {
-				filters: {
-					issingle: 0,
-					istable: 0,
-				},
-			};
-		});
+		// frm.set_query("ref_doctype", "producer_doctypes", function () {
+		// 	return {
+		// 		filters: {
+		// 			issingle: 0,
+		// 			istable: 0,
+		// 		},
+		// 	};
+		// });
 
 		frm.set_indicator_formatter("status", function (doc) {
 			let indicator = "orange";
