e9c6d1aed1b3c0d4afd44f5a02707c60b905d342
AhmedAbokhatwa <ahmedreda199785@gmail.com>
2025-01-26 10:32:21 +0200

    move client script to background branch connection

--- branches_connection/branche_connection/doctype/branch_connecting/branch_connecting.js
+++ branches_connection/branche_connection/doctype/branch_connecting/branch_connecting.js
@@ -3,7 +3,7 @@
 frappe.ui.form.on("Branch Connecting", {
   refresh(frm) {
       // Add a custom button to initiate connection
-      // frm.add_custom_button("Create Connection", () => {
+      frm.add_custom_button("Create Connection", () => {
           frm.call({
               method: "create_connection",
               doc: frm.doc, // Automatically sends the current document context
@@ -15,6 +15,20 @@ frappe.ui.form.on("Branch Connecting", {
                   }
               }
           });
-      // });
+      });
   },
 });
+////////////////////////////////////////////indictor for list view//////////////////////////
+////////////////////////////////////////////indictor for list view//////////////////////////
+////////////////////////////////////////////indictor for list view//////////////////////////
+frappe.listview_settings['Branch Connecting'] = {
+  get_indicator(doc) {
+      // Customize indicator color for each connection
+      if (doc.connection == "Connected") {
+          return [__("Connected"), "blue", "connection,=,Connected"];
+      } else if (doc.connection == "DisConnected") {
+          return [__("DisConnected"), "red", "connection,=,DisConnected"];
+      }
+  
+  },
+};
